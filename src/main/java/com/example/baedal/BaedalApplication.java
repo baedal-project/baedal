94a4ef157e6d2233860e7399e62f421ca05b42d1
irupa94 <irupa94@naver.com>
2022-11-17 13:14:39 +0900

    init : 프로젝트 시작

---
+++ src/main/java/com/example/baedal/BaedalApplication.java
@@ -0,0 +1,13 @@
+package com.example.baedal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaedalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaedalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baedal/BaedalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
