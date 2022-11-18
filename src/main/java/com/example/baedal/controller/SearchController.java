28869ac93c3a3f0a435b8e1da70d342a73c3271e
Puri12 <tkdqja9812@gmail.com>
2022-11-18 19:19:21 +0900

    feat : 멤버 주소별 음식점 이름 검색 API
    - 검색 API 구현
    {
        "keyword": "검색어",
        "memberId": 멤버Id
    }
    
    Issue: #16

---
+++ src/main/java/com/example/baedal/controller/SearchController.java
@@ -0,0 +1,21 @@
+package com.example.baedal.controller;
+
+import com.example.baedal.dto.request.SearchRequestDto;
+import com.example.baedal.dto.response.ResponseDto;
+import com.example.baedal.service.SearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SearchController {
+
+    private final SearchService searchService;
+
+    @PostMapping(value = "/api/search")
+    public ResponseDto<?> postOrder(@RequestBody SearchRequestDto requestDto) {
+        return searchService.search(requestDto);
+    }
+}
