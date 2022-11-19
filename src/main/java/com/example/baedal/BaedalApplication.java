fbd758cf43d837ce564a4286c6d15bf74fb26f06
celina <celinaym0810@gmail.com>
2022-11-19 18:49:23 +0900

    fix : application에 commandLineRunner 주석처리
    -

--- src/main/java/com/example/baedal/BaedalApplication.java
+++ src/main/java/com/example/baedal/BaedalApplication.java
@@ -33,18 +33,18 @@ public class BaedalApplication {
 //        };
 //    }
     
-    @Bean
-    public CommandLineRunner demo(ItemRepository itemRepository) {
-        return (args) -> {
-            //storeRepository.save(new Store(1,"족발집","jecheon-si",Category.JOCKBAL,"족발집"));
-            //itemRepository.save(Item.builder().price(1500).category(Category.JOCKBAL).name("치킨").build());
-            itemRepository.save(Item.builder().price(1500).category(Category.JOCKBAL).name("족발").store(Store.builder().storeId(1L).build()).build());
-            itemRepository.save(Item.builder().price(1500).category(Category.JOCKBAL).name("족발").store(Store.builder().storeId(1L).build()).build());
-            itemRepository.save(Item.builder().price(2500).category(Category.JOCKBAL).name("족발").store(Store.builder().storeId(2L).build()).build());
-            itemRepository.save(Item.builder().price(3000).category(Category.JOCKBAL).name("족발").store(Store.builder().storeId(2L).build()).build());
-
-        };
-    }
+//    @Bean
+//    public CommandLineRunner demo(ItemRepository itemRepository) {
+//        return (args) -> {
+//            //storeRepository.save(new Store(1,"족발집","jecheon-si",Category.JOCKBAL,"족발집"));
+//            //itemRepository.save(Item.builder().price(1500).category(Category.JOCKBAL).name("치킨").build());
+//            itemRepository.save(Item.builder().price(1500).category(Category.JOCKBAL).name("족발").store(Store.builder().storeId(1L).build()).build());
+//            itemRepository.save(Item.builder().price(1500).category(Category.JOCKBAL).name("족발").store(Store.builder().storeId(1L).build()).build());
+//            itemRepository.save(Item.builder().price(2500).category(Category.JOCKBAL).name("족발").store(Store.builder().storeId(2L).build()).build());
+//            itemRepository.save(Item.builder().price(3000).category(Category.JOCKBAL).name("족발").store(Store.builder().storeId(2L).build()).build());
+//
+//        };
+//    }
 
 //    @Bean
 //    public CommandLineRunner demo(OrderRepository orderRepository) {
