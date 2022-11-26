a5c649d8df60c8141b2f2a693e050680290fc576
celina <celinaym0810@gmail.com>
2022-11-26 14:34:36 +0900

    bug : test cases 주석
    -

--- src/test/java/com/example/baedal/repository/OrderRepository/OrderRepositoryImplTest.java
+++ src/test/java/com/example/baedal/repository/OrderRepository/OrderRepositoryImplTest.java
@@ -1,60 +1,40 @@
-package com.example.baedal.repository.OrderRepository;
+//package com.example.baedal.repository.OrderRepository;
 
-import com.example.baedal.domain.OrderHasItem;
-import com.example.baedal.domain.Orders;
-import com.example.baedal.dto.response.AllOrderResponseDto;
-import com.example.baedal.dto.response.OrderResponseDto;
-import com.example.baedal.dto.response.QAllOrderResponseDto;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+//import com.example.baedal.domain.OrderHasItem;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
-import java.util.List;
+//@SpringBootTest
+//@Transactional
+//@DisplayName("order 관련 test")
+//class OrderRepositoryImplTest {
 
-import static com.example.baedal.domain.QOrderHasItem.orderHasItem;
-import static org.assertj.core.api.Assertions.assertThat;
+//    @PersistenceContext
+//    EntityManager em;
+//
+//    @Autowired
+//    private OrderRepository orderRepository;
+//
+//    JPAQueryFactory queryFactory;
+//
+//    @BeforeEach
+//    public void before(){
+//        queryFactory = new JPAQueryFactory(em);
+//    }
 
-@SpringBootTest
-@Transactional
-@DisplayName("order 관련 test")
-class OrderRepositoryImplTest {
+//    @Test
+//    @DisplayName("전체 주문개수 맞나 확인")
+//    public void getAllOrder(){
+//        //test cases들은 구현체와 최대한 결합도가 낮아야 good
+//        //List<AllOrderResponseDto> allOrders = orderRepository.getAllOrder();
+//        //List<OrderHasItem> allOrders = orderRepository.getAllOrder();
+//        //List<Orders> allOrders = orderRepository.getAllOrder();
+//        //assertThat(allOrders.size()).isEqualTo(12);
+//    }
 
-    @PersistenceContext
-    EntityManager em;
-
-    @Autowired
-    private OrderRepository orderRepository;
-
-    JPAQueryFactory queryFactory;
-
-    @BeforeEach
-    public void before(){
-        queryFactory = new JPAQueryFactory(em);
-    }
-
-    @Test
-    @DisplayName("전체 주문개수 맞나 확인")
-    public void getAllOrder(){
-        //test cases들은 구현체와 최대한 결합도가 낮아야 good
-        //List<AllOrderResponseDto> allOrders = orderRepository.getAllOrder();
-        //List<OrderHasItem> allOrders = orderRepository.getAllOrder();
-        List<Orders> allOrders = orderRepository.getAllOrder();
-        assertThat(allOrders.size()).isEqualTo(12);
-    }
-
-    @Test
-    @DisplayName("주문 낱개로 잘 갖고 오나 확인")
-    public void getOneOrder(){
-        Orders oneOrder = orderRepository.getOneOrder(1L);
+//    @Test
+//    @DisplayName("주문 낱개로 잘 갖고 오나 확인")
+//    public void getOneOrder(){
+//        Orders oneOrder = orderRepository.getOneOrder(1L);
         //src 내용이랑 너무 의존성이 강함
 //        assertThat(oneOrder.getAmount().equals(5));
 //        assertThat(oneOrder.get(0).getStoreId().equals(1L));
@@ -63,8 +43,8 @@ class OrderRepositoryImplTest {
 //        assertThat(oneOrder.get(0).getMemberId().equals(1L));
 //        assertThat(oneOrder.get(0).getMemberName().equals("yeongmin1"));
 
-    }
+//    }
 
 
 
-}
+//}
